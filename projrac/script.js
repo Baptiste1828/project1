d07c48f862bb7e8eea4cfee40205064042890f41
27rachiid82 <mohammedikacirachid@yahoo.fr>
2023-10-01 19:39:38 +0200

    add diapo auto dans fichiers html css js

--- projrac/script.js
+++ projrac/script.js
@@ -6,3 +6,20 @@ document.addEventListener("submit", function (event) {
     alert(message);
 }
 );
+
+
+//TEST DIAPO
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 2800); // Change image every 2 seconds
+}
